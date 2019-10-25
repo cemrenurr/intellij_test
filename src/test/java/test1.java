d892a32d9c7bf798ca652bf27a491bf8402b93e3
Cemrenur Topuz <cemre.topuz@gmail.com>
2019-10-25 18:11:00 -0400

    19 edit time

--- src/test/java/test1.java
+++ src/test/java/test1.java
@@ -2,5 +2,6 @@ public class test1 {
     public static void main(String[] args) {
         System.out.println("print hello");
         System.out.println("print");
+        System.out.println("print2");
     }
 }
